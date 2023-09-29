6aaa0b813581b1eb2bda944c698c7396961b53d8
official-david <davidadigwu@gmail.com>
2023-09-29 20:27:20 +0100

    fix

--- database/migrations/2023_09_29_181947_create_comments_table.php
+++ database/migrations/2023_09_29_181947_create_comments_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignIdFor('user_id');
+            $table->foreignIdFor(User::class);
             $table->foreignUuid('ticket_id');
             $table->longText('text');
             $table->timestamps();
